4c98f29a46ed45f618ec08536aff7dc1be57e218
big-yogurt <245382579+big-yogurt@users.noreply.github.com>
2026-06-27 19:35:24 +0500

    Добавлены заготовки функций для общения с другими сервисами

--- main.py
+++ main.py
@@ -1,32 +1,49 @@
+import logging
+
+import torch
+import numpy as np
 import segmentation_models_pytorch as smp
-from PIL.Image import Image
-
-from synthetic import DMSyntheticDataset
-## Для отрисовки в картинку.
-import torchvision.transforms as T
-
-
 
 def main():
-    # model = smp.UnetPlusPlus(
-    #     # TODO: Нужно поиграться с настройками
-    #     encoder_name="efficientnet-b3",
-    #     encoder_weights=None,
-    #     in_channels=3,
-    #     classes=1,
-    #     activation=None,
-    # )
+    log = logging.getLogger(__name__)
+    client = None
+    try:
+        client = listen_port(1234)
+    except Exception:
+        log.critical()
 
-    ## Генерим рандомный экземпляр и сохраняем его.
-    synth = DMSyntheticDataset()
-    data = synth[0]
+    model = smp.UnetPlusPlus(
+        # TODO: Нужно поиграться с настройками
+        encoder_name="efficientnet-b3",
+        encoder_weights=None,
+        in_channels=3,
+        classes=1,
+        activation=None,
+    )
+    while True:
+        img = recv_image(client)
+        recovered_img = recover_image(model, img)
+        send_image(client)
 
-    input: Image = T.ToPILImage()(data["input_tensor"])
-    target: Image = T.ToPILImage()(data["target_tensor"])
 
-    # display the PIL image
-    input.show("in")
-    target.show("out")
+def recv_image(client: ) -> np.ndarray:
+    """
+    Принимает изображение от клиента (сервиса распознавания), переводит его в
+    numpy.ndarray и возвращает
+    """
+    ...
+
+def send_image(client: , img: np.ndarray):
+    """
+    Отправляет изображение клиенту
+    """
+    ...
+
+def recover_image(model: torch.nn.Module, img: np.ndarray) -> np.ndarray:
+    """
+    Восстанавливает изображение 'img' через нейросеть 'model'
+    """
+    ...
 
 if __name__ == "__main__":
     main()
