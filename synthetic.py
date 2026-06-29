d590ccb42fb6939e7d4be8f196602717b6b42f0d
big-yogurt <245382579+big-yogurt@users.noreply.github.com>
2026-06-29 20:07:20 +0500

    Переписана реализация синтетического датасета

--- synthetic.py
+++ synthetic.py
@@ -1,8 +1,7 @@
 import cv2
-import numpy as np
-import torch
 from torch import Tensor
 from torch.utils.data import Dataset
+from torchvision.transforms.functional import to_tensor
 
 import imggen
 
@@ -30,12 +29,7 @@ class DMSyntheticDataset(Dataset):
         ideal_img = imggen.gen_random_dmtx()
         damage_img = imggen.make_img_realistic(ideal_img)
         ideal_gray = cv2.cvtColor(ideal_img, cv2.COLOR_RGB2GRAY)
-        mask = torch \
-            .from_numpy(ideal_gray.astype(np.float32) / 255.0) \
-            .unsqueeze(0)                                     \
-            .float()
-        img = torch \
-            .from_numpy(damage_img.astype(np.float32) / 255.0) \
-            .permute(2, 0, 1)                                  \
-            .float()
+        mask = to_tensor(ideal_gray)
+        img = to_tensor(damage_img)
+        
         return img, mask
