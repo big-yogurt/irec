4c8e1704cd5d4a1a9a04151bcc04ae0329cd4888
big-yogurt <245382579+big-yogurt@users.noreply.github.com>
2026-06-29 19:36:46 +0500

    Разделение генератора и синтетического датасета

---
+++ imggen.py
@@ -0,0 +1,88 @@
+"""
+Для обучения нейросети требуется датасет с различными изображениями. Создать
+датасет с реальными фотографиями тяжело, поэтому было принято решение создать
+генератор изображений datamatrix'ов.
+
+В файле, помимо генерации datamatrix'ов, есть код для ухудшения качества
+изображения, чтобы нейросеть училась исправлять дефекты и выдавать чистый
+datamatrix.
+"""
+
+
+import random
+
+import cv2
+import numpy as np
+import albumentations as A
+import pylibdmtx.pylibdmtx as dmtx
+
+
+DEFAULT_IMG_SIZE = (256, 256)
+
+
+def gen_random_dmtx(max_data_len=64) -> np.ndarray:
+    """
+    Генерирует datamatrix со случайными закодироваными данными. Возвращает в
+    виде `np.ndarray` в RGB.
+    Эта функция выдаёт маски, которые нейросеть должна давать на выходе.
+    """
+    data_len = random.randint(1, max_data_len)
+    data = random.randbytes(data_len)
+    encoded = dmtx.encode(data)
+    img = np.frombuffer(encoded.pixels, dtype=np.uint8)
+    img = img.reshape(encoded.height, encoded.width, 3)
+    img = cv2.resize(img, (256, 256), interpolation=cv2.INTER_AREA)
+    return img
+
+
+def make_img_realistic(img: np.ndarray) -> np.ndarray:
+    """
+    Добавляет к `img` фон (предполагается, что изображение чёрно-белое. Белый
+    фон будет заменён на сгенерировный или заготовленное изображение). Так же
+    случайным образом будут добавлены царапины, замыливание и др. дефекты,
+    которые мешают сканированию datamatrix'а.
+    Эта функция выдаёт изображения, которые нейросеть будет принимать на входе.
+    """
+    img = _add_random_background_to_img(img)
+    final_transform = A.Compose([
+        A.RandomBrightnessContrast(),
+        A.RandomToneCurve(),
+        A.MotionBlur(blur_limit=9, p=0.7),
+        A.RandomSunFlare(src_radius=200),
+    ])
+    img = final_transform(image=img)['image']
+    return img
+
+
+def _add_random_background_to_img(img: np.ndarray) -> np.ndarray:
+    bg = _gen_img_background()
+    h_code, w_code = img.shape[:2]
+    roi = bg[0:h_code, 0:w_code]
+    mask = (img[:, :, 0] == 0)
+    roi[mask] = img[mask]
+    bg[0:h_code, 0:w_code] = roi
+    return bg
+
+
+def _gen_img_background() -> np.ndarray:
+    h, w = DEFAULT_IMG_SIZE
+    n = random.randint(0, 2)
+    if n == 0: # Металл
+        noise = np.random.normal(128, 30, (h, w)).astype(np.uint8)
+        kernel = np.ones((1, 5), np.float32) / 5
+        tex = cv2.filter2D(noise, -1, kernel)
+        tex = cv2.cvtColor(tex, cv2.COLOR_GRAY2BGR)
+        grad = np.linspace(0.8, 1.2, w).reshape(1, w, 1)
+        tex = (tex * grad).astype(np.uint8)
+        return tex
+    elif n == 1: # Бумага
+        return np.random.normal(220, 6, (h, w, 3)).astype(np.uint8)
+    return np.random.normal(180, 10, (h, w, 3)).astype(np.uint8)
+
+
+if __name__ == "__main__":
+    for i in range(10):
+        mask = gen_random_dmtx()
+        img = make_img_realistic(mask)
+        cv2.imwrite(f"images/img{i}.png", img)
+        cv2.imwrite(f"images/mask{i}.png", mask)
--- synthetic.py
+++ synthetic.py
@@ -1,15 +1,10 @@
-import random
-
 import cv2
 import numpy as np
-import albumentations as A
-import pylibdmtx.pylibdmtx as dmtx
 import torch
 from torch import Tensor
 from torch.utils.data import Dataset
 
-
-DEFAULT_IMG_SIZE = (256, 256)
+import imggen
 
 
 class DMSyntheticDataset(Dataset):
@@ -32,20 +27,9 @@ class DMSyntheticDataset(Dataset):
         Возвращает синтетические данные (изображения DM кодов в виде тензоров).
         Первый тензор - входное изображение, второй - маска сегментации.
         """
-        bytes_count = random.randint(1, 64)
-        data = random.randbytes(bytes_count)
-        dmtx_img = dmtx.encode(data)
-
-
-        ideal_img = np \
-            .frombuffer(dmtx_img.pixels, dtype=np.uint8) \
-            .reshape(dmtx_img.height, dmtx_img.width, 3)
-        ideal_img = cv2 \
-            .resize(ideal_img, (256, 256), interpolation=cv2.INTER_AREA)
-        damage_img = _add_random_damage_to_img(ideal_img)
-
+        ideal_img = imggen.gen_random_dmtx()
+        damage_img = imggen.make_img_realistic(ideal_img)
         ideal_gray = cv2.cvtColor(ideal_img, cv2.COLOR_RGB2GRAY)
-
         mask = torch \
             .from_numpy(ideal_gray.astype(np.float32) / 255.0) \
             .unsqueeze(0)                                     \
@@ -55,56 +39,3 @@ class DMSyntheticDataset(Dataset):
             .permute(2, 0, 1)                                  \
             .float()
         return img, mask
-
-
-
-def _add_random_damage_to_img(ideal_img: np.ndarray) -> np.ndarray:
-    """
-    'Портит' изображение, добавляя различные помехи.
-    """
-    img = _add_random_background_to_img(ideal_img)
-    final_transform = A.Compose([
-        A.RandomBrightnessContrast(),
-        A.RandomToneCurve(),
-        A.MotionBlur(blur_limit=9, p=0.7),
-        A.RandomSunFlare(src_radius=200),
-    ])
-    img = final_transform(image=img)['image']
-    return img
-
-
-def _add_random_background_to_img(img: np.ndarray) -> np.ndarray:
-    bg = _gen_img_background()
-    h_code, w_code = img.shape[:2]
-    roi = bg[0:h_code, 0:w_code]
-    mask = (img[:, :, 0] == 0)
-    roi[mask] = img[mask]
-    bg[0:h_code, 0:w_code] = roi
-    return bg
-
-
-def _gen_img_background() -> np.ndarray:
-    h, w = DEFAULT_IMG_SIZE
-    n = random.randint(0, 2)
-    if n == 0: # Металл
-        noise = np.random.normal(128, 30, (h, w)).astype(np.uint8)
-        kernel = np.ones((1, 5), np.float32) / 5
-        tex = cv2.filter2D(noise, -1, kernel)
-        tex = cv2.cvtColor(tex, cv2.COLOR_GRAY2BGR)
-        grad = np.linspace(0.8, 1.2, w).reshape(1, w, 1)
-        tex = (tex * grad).astype(np.uint8)
-        return tex
-    elif n == 1: # Бумага
-        return np.random.normal(220, 6, (h, w, 3)).astype(np.uint8)
-    return np.random.normal(180, 10, (h, w, 3)).astype(np.uint8)
-
-
-if __name__ == "__main__":
-    dataset = DMSyntheticDataset(dataset_len=10)
-    for i in range(10):
-        img, mask = dataset[0]
-        img, mask = (img.permute(2, 0, 1).numpy() * 255) \
-            .astype(np.uint8), (mask.squeeze(0).numpy() * 255).astype(np.uint8)
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-        cv2.imwrite(f"images/img{i}.png", img)
-        cv2.imwrite(f"images/mask{i}.png", mask)
