aa08ce80ab011797dea61893d8698d7db8123111
big-yogurt <245382579+big-yogurt@users.noreply.github.com>
2026-06-30 16:05:20 +0500

    Добавлена таблица loss функций

--- loss.py
+++ loss.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 from torch import Tensor
 
 
@@ -17,3 +18,31 @@ def hard_loss(predict, target) -> Tensor:
     uncertainty_penalty = uncertainty.mean()
 
     return bce + 0.5 * uncertainty_penalty
+
+
+loss_function_table = {
+    # Самописные реализации loss функции
+    "hard_loss": hard_loss,
+
+    # Реализации loss функции из pytorch
+    "L1Loss": nn.L1Loss,
+    "MSELoss": nn.MSELoss,
+    "CrossEntropyLoss": nn.CrossEntropyLoss,
+    "NLLLoss": nn.NLLLoss,
+    "BCELoss": nn.BCELoss,
+    "BCEWithLogitsLoss": nn.BCEWithLogitsLoss,
+    "HuberLoss": nn.HuberLoss,
+    "SmoothL1Loss": nn.SmoothL1Loss,
+    "KLDivLoss": nn.KLDivLoss,
+    "CTCLoss": nn.CTCLoss,
+    "PoissonNLLLoss": nn.PoissonNLLLoss,
+    "MarginRankingLoss": nn.MarginRankingLoss,
+    "HingeEmbeddingLoss": nn.HingeEmbeddingLoss,
+    "CosineEmbeddingLoss": nn.CosineEmbeddingLoss,
+    "MultiMarginLoss": nn.MultiMarginLoss,
+    "MultiLabelMarginLoss": nn.MultiLabelMarginLoss,
+    "MultiLabelSoftMarginLoss": nn.MultiLabelSoftMarginLoss,
+    "SoftMarginLoss": nn.SoftMarginLoss,
+    "TripletMarginLoss": nn.TripletMarginLoss,
+    "TripletMarginWithDistanceLoss": nn.TripletMarginWithDistanceLoss,
+}
