42352e021d6f0ae7f8a934896d271e2d2e7382ec
big-yogurt <245382579+big-yogurt@users.noreply.github.com>
2026-06-30 15:41:05 +0500

    Перенос import для избежания не нужной загрузки библиотек

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 
 import cli
-import train
 
 
 def main():
@@ -25,6 +24,7 @@ def command_run(cli_flags: argparse.Namespace):
 
 
 def command_syn_train(cli_flags: argparse.Namespace):
+    import train
     model = train.DMTrainModel()
     model.start_training(cli_flags.epoch, cli_flags.dataset_len)
     model.save(cli_flags.save_nn)
@@ -35,12 +35,14 @@ def command_train(cli_flags: argparse.Namespace):
 
 
 def command_test(cli_flags: argparse.Namespace):
+    import train
     model = train.DMTrainModel()
     model.load(cli_flags.load_nn)
     model.test_img(cli_flags.path_to_image)
 
 
 def command_syn_test(cli_flags: argparse.Namespace):
+    import train
     model = train.DMTrainModel()
     model.load(cli_flags.load_nn)
     model.test()
